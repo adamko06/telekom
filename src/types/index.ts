47b71d47a51050ea0430070a5a9708ac7d683878
Adam Kozusnik <adam.kozusnik@seznam.cz>
2025-06-30 23:07:01 +0200

    fix types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,17 +1,7 @@
-import type { components, operations } from './generated'
-
 export type { components, operations, paths } from './generated'
-export type { ProductFilters }
-
-export type Product = components['schemas']['Product']
-export type ProductsResponse =
-  operations['getAllProducts']['responses']['200']['content']['application/json']
-export type ProductResponse =
-  operations['getProductById']['responses']['200']['content']['application/json']
-export type CategoriesResponse = string[]
-
-type ProductFilters = {
-  minPrice?: number
-  maxPrice?: number
-  category?: string
-}
+export type {
+  CategoriesResponse,
+  Product,
+  ProductResponse,
+  ProductsResponse,
+} from './types'
