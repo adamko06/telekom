c6c614758b5827af21f3b80a1d35624ed2fc33fb
Adam Kozusnik <adam.kozusnik@seznam.cz>
2025-07-02 21:43:38 +0200

    make less cols

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -20,7 +20,7 @@ const ProductGrid = ({ products }: ProductGridProps) => {
   }
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-6">
       {products.map(product => (
         <ProductCard key={product.id} product={product} />
       ))}
